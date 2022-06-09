94706734352cd67ac9aee517d776d938a6771bb8
Dae-Day <90940039+Dae-Day@users.noreply.github.com>
2022-06-09 13:17:28 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
-int main(void)
+;int main(void)
 {
-	char x[] = _putchar(char c);
+	char x[] = "_putchar";
 	int i;
 
-	for (i = 0; i < 8; i++)
+	for (i = 0; i < 8; i++);
 	{
 		_putchar(x[i]);
 	}
