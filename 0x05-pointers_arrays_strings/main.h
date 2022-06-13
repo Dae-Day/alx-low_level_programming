beb3ece1c8a2186a49779c1598c4be67137c691e
Dae-Day <casssade212@gmail.com>
2022-06-13 13:02:46 -0700

    update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,19 +1,21 @@
-#include <stdlib.h>
+#include <stdio.h>
 
-int _putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-size_t _strlen(const char *str);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _atoi(char *s);
+void swap_int(int *, int *);
 
-<<<<<<< HEAD
-=======
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a;
+	int b;
 
->>>>>>> 3ea9fbc54ea4db395142a9bfe561c02e3d6839a2
+	a = 98;
+	b = 42;
+	printf("%d, %d\n", a, b);
+	swap_int(&a, &b);
+	printf("%d, %d\n", a, b);
+	return (0);
+}
