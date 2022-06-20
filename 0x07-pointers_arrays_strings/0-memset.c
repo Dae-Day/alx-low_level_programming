75428cd1e1693bdfec8b9000c59ec134d117593e
Dae-Day <casssade212@gmail.com>
2022-06-20 09:01:00 -0700

    0. memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,28 @@
 #include "main.h"
 
 /**
- * memset - Fills the first n bytes of the memory area pointed to by @s with the constant byte @c.
- * @S - pointer to the memory area
- * @c - character to fill the memory area with.
- * @n - Number of bytes.
- *
- * Return: Pointer to the memory area @s.
- */
-char *_memset(char *s, char b, unsigned int n)
+* _strncat - concats two arrays
+*
+* @dest: destination of concat
+* @src: source array to concat
+* @n: amount of times to append
+*Return: char value
+*/
+
+char *_strncat(char *dest, char *src, int n)
 {
-	unsigned int i;
+	int i = 0;
+	int j;
 
-	for (i = 0; i < n; i++)
-		s[i] = b;
-
-	return (s);
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j < n && src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	return (dest);
 }
+
