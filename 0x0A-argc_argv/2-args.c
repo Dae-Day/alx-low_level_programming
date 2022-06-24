40d66d8a27c594e4e84a56ef08132ff1b4d501d5
Dae-Day <casssade212@gmail.com>
2022-06-24 01:49:57 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - print argv arguments received.
+ * @argc: array length.
+ * @argv: array of pointers.
+ *
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	while (*(argv))
+	{
+		printf("%s\n", *(argv));
+		argv++;
+	}
+
+	return (0);
+}
