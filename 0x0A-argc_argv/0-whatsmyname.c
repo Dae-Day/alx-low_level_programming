1215062fa052ba24a3ec64c65f79617991730957
Dae-Day <casssade212@gmail.com>
2022-06-24 01:43:30 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the program name,followed by a new line.
+ * @argc: Number of arguments.
+ * @argv: Array of pointers
+ *
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", *argv);
+
+	exit(EXIT_SUCCESS);
+}
