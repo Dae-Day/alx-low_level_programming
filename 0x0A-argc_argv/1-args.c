1ecc430ef386e224869dd422ad2c76cb629a6708
Dae-Day <casssade212@gmail.com>
2022-06-24 05:20:19 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,24 +1,18 @@
 #include <stdio.h>
-#include "main.h"
+
 /**
- * main -prints the number of arguments.
- * @argc: array length.
- * @argv: array.
- *
- * Return: 0.
+ * main - Prints the number of arguments passed into thre function
+ * @argc: The argument count
+ * @argv: The argument vector
+ * Return: Always 0
  */
+
 int main(int argc, char *argv[])
 {
-	int i;
+	(void)argv;
 
-	if (argc == 1)
-		printf("%d\n", argc - 1);
-	else
-	{
-		for (i = 0; *argv; i++, argv++);
-
-		printf("%d\n", i - 1);
-	}
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
+
