602b264a58caaa08b229094ef7167a2806bfb3dc
Dae-Day <casssade212@gmail.com>
2022-07-25 13:12:32 -0700

    3-pc.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,9 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *create_buffer(char *file);
+void close_file(int fd);
 
 /**
  * main - copies the content of a file to another file
